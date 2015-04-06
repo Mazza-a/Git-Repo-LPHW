b0a1fd814375fdc86b75cde02bdb3fb37b120bfb
Mazza-a <marioajayi12@me.com>
2015-04-06 14:37:58 -0700

    Added Excercise 7 from Learn Python the Hard Way
    weve done 7 out of 52 Yay!!

---
+++ ex7.py
@@ -0,0 +1,25 @@
+print "Mary had a little lamb"
+#Printing a format string with the formatted value being the string "snow"
+print "It's fleece was white as %s." % "snow"
+print "And everywhere that Mary went."
+# multiplying a string containing a full stop by 10 to create a dotted line
+print "." * 10
+#assigning values of individual letters to a number of variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+""""adding all of the variables together to spell out "Cheese Burger"
+and printing it two different lines but using (,) to join the
+two print statements to one line"""
+print end1 + end2 + end3 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
