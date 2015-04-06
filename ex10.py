558c90254c9188bbbd66e38b99ced866696d4f6e
Mazza-a <marioajayi12@me.com>
2015-04-06 16:08:51 -0700

    Added Exercise 10 of Learn Python The Hard Way
    Almost 20% of the book completed

---
+++ ex10.py
@@ -0,0 +1,19 @@
+# defines a variable that tabs a string using \t
+tabby_cat = "\tI'm tabbed in."
+# Defines a variable that splits a string to the next line after \n
+persian_cat = "I'm split\non a line"
+# Defines a variable that separates each word with \ using double backslashes \\
+backslash_cat = "I'm \\ a \\ cat"
+#defines a variable that makes a list using triple quotes and the \t that tabs the each new line
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+#prints the value of each variable
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
