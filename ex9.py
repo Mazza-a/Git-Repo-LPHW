59a176f1124b8fd3de7b6d1b642ff191cd93d6f8
Mazza-a <marioajayi12@me.com>
2015-04-06 15:47:12 -0700

    Added the Exercise 9 from Learn Python The Hard Way
    Were 17% through
    and i realise i've spelt exercise wrong all this time

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# defining a days variable with a single string containing the days of the week
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# defining a variable with a single string containing the months in a year up to august seperated by \n
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
