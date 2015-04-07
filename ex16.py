393467138688b736dc9056fd4f37272cc6b973dd
Mazza-a <marioajayi12@me.com>
2015-04-06 19:27:47 -0700

    Added Exercise 16 from Learn Python The Hard Way
    17 0ut of 52, Almost to 20!!!

--- ex16.py
+++ ex16.py
@@ -1,27 +1,32 @@
 from sys import argv
 
+#assigns two arguments to argv
 script, filename = argv
 
+#first prints a format string using our filename argument, then prints out a question for the raw_input
 print "We're going to erase %r." % filename
 print "If you don't want that, hit CTRL-C (^C)"
 print "If you do want that, hit RETURN."
-
 raw_input("?")
 
+#prints open file and then opens the file in write mode
 print "Opening the file..."
 target = open(filename, 'w')
 
+#Truncates the file(deletes every thing in the file)
 print "Truncating the file.   Goodbye!"
 target.truncate()
 
 print "Now I'm going to ask you for three lines"
 
+#asks the user for three lines and stores them in separate variables
 line1 = raw_input("line 1: ")
 line2 = raw_input("line 2: ")
 line3 = raw_input("line 3: ")
 
 print "I'm going to write to the file."
 
+#Writes what the user wrote from the 3 raw_inputs to the blank file
 target.write(line1)
 target.write("\n")
 target.write(line2)
@@ -29,5 +34,6 @@ target.write("\n")
 target.write(line3)
 target.write("\n")
 
+# Closes the file after writing the text
 print "And finally, we close it."
 target.close()
