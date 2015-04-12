a6b10ad8286afdfe7cc2d4c0fbdde804c4fef74b
Mazza-a <marioajayi12@me.com>
2015-04-12 16:14:59 -0700

    Added Exercise 33 from LPHW and creted a function out of it so it is also is in .pyc form
    Did the study drills for ex33 and made it into a module

---
+++ ex33.py
@@ -0,0 +1,18 @@
+
+def loopy(n):
+	i = 0
+	numbers = []
+	while i < n:
+		print "At the top the i  is %d" % i
+		numbers.append(i)
+		
+		i += 1
+		print "Numbers now: ", numbers
+		print "At the bottom i is %d" % i
+		
+
+	print "The numbers: "
+
+	for num in numbers:
+		print num
+		
