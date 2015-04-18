c2da5d1d3c6e0a83b02a539e78ef9311a846f961
Mazza-a <marioajayi12@me.com>
2015-04-18 13:00:10 -0700

    Added Exercise 34 from Learn Python The Hard Way
    34 out of 52 exercises completed!!!!

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ["bear", "python", "peacock", "kangaroo", "whale", "platypus"]
+
+print ("The animal at 1 is the second animal and is a %s.") % animals[1]
+print ("The 3rd animal is at 2 and is a %s.") % animals[2]
+print ("The 1st animal is at 0 and is a %s.") % animals[0]
+print ("The animal at 3 is the fourth animal and is a %s.") % animals[3]
+print ("The 5th animal is at 6 and is a %s.") % animals[4]
+print ("The animal at 2 is the third animal and is a %s.") % animals[2]
+print ("The 6th animal is at 7 and is a %s.") % animals[5]
+print ("The animal at 4 is the fifth animal and is a %s") % animals[4]
